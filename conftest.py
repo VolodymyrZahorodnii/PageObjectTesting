2309929dced452c56d751996dc28c048ec574ba0
VZahorodniiImp <zagorodniy.vovik@gmail.com>
2022-03-16 22:20:46 +0200

    Adding new files to the environment

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language= ru, n, fi, de, en-gb, fr")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    options = Options()
+    user_language = request.config.getoption("language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print('\nQuit browser')
+    browser.quit()
