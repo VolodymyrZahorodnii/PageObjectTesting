ffa9f6168e8724a3952df8ca6559f63de0109a54
VZahorodniiImp <zagorodniy.vovik@gmail.com>
2022-03-30 21:56:05 +0300

    Updating the overall structure of my tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,22 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element_by_css_selector("#login_link")
+        link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.ID, "login_link"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_authorization_page(self):
+        authorization_link = self.is_element_present(*MainPageLocators.LOGIN_LINK)
+        authorization_link.click()
 
     def is_element_present(self, how, what):
         try:
